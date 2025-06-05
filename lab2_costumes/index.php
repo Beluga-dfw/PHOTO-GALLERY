981813b136708ae7d7f0f049f7149b572bc57a0d
Beluga-dfw <beluga2777@gmail.com>
2025-06-05 15:25:57 +0200

    Delete lab2_costumes directory

--- lab2_costumes/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'controllers/Index_costume_Controller.php';
-
-$controller = new CostumeController();
-$costumes = $controller->getCostumes();
-
-include 'views/Index_Costume.php';
-?>
--- lab2_costumes/models/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("sqlite:database.sqlite");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo " Підключення до бази даних успішне!";
-} catch (PDOException $e) {
-    die(" Помилка з'єднання: " . $e->getMessage());
-}
